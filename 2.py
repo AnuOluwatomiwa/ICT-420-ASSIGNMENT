c910cb9e645dfb96d971e1581b402aa12f866eac
Israel <bamideleisrael04@gmail.com>
2024-04-30 09:58:15 +0100

    Add plot for signal f(t) = 0.8A * cos(ωt)
    - Implemented code to plot f(t) against t from t = 0 to t = 2T
    - Plotted signal using time points T/6, T/3, ..., 2T
    - Included additional code to mark sample points

---
+++ 2.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Constants
+T = 10e-6  # 10 microseconds
+A = 26  # Last two digits of matriculation number
+ω = 2 * np.pi / T
+
+# Define time points
+time_points = np.array([T/6, T/3, T/2, 2 * T/3, 5 * T/6, T, 7 * T/6, 4 * T/3, 3 * T/2, 5 * T/3, 11 * T/6, 2 * T])
+t = np.linspace(0, 2 * T, 1000)
+
+# Signal f(t)
+f_t = 0.8 * A * np.cos(ω * t)
+
+# Create the plot
+plt.figure(figsize=(10, 6))
+plt.plot(t, f_t, label='f(t) = 0.8A*cos(ωt)', color='b', linestyle='-', linewidth=2)
+plt.scatter(time_points, 0.8 * A * np.cos(ω * time_points), color='r', label='Sample Points')
+
+plt.xlabel('t (seconds)')
+plt.ylabel('f(t) (volts)')
+plt.title('Plot of f(t) against t from t = 0 to t = 2T')
+plt.legend()
+plt.grid(True)
+plt.show()
